6f5ed1a7633a7e7ed64c0d408b165099424e2e84
samuelthaiyil <77406282+samuelthaiyil@users.noreply.github.com>
2021-12-02 18:51:38 -0500

    Added app logo

--- src/main/java/com/example/cryptopricewatcher/Main.java
+++ src/main/java/com/example/cryptopricewatcher/Main.java
@@ -3,6 +3,7 @@ package com.example.cryptopricewatcher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("eth-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Crypto Price Watcher");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/eth_logo.png")));
         stage.setScene(scene);
         stage.show();
     }
